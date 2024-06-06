d765d4593844e86d26d6868aeaf66adba4eed8e0
harshhp8 <harshhp8@users.noreply.github.com>
2024-06-06 23:34:16 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -51,7 +51,7 @@ load_dotenv()
 
 li=[]
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
