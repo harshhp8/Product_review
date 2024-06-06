1d403f1ec9276127ff4ec15b7d4a68c834df34e8
harshhp8 <harshhp8@users.noreply.github.com>
2024-06-06 23:07:35 +0530

    product_info file

---
+++ stories_json.py
@@ -0,0 +1,82 @@
+product_json=''' 
+
+SKU
+Product Name
+Category
+Product Tier
+Full Class Name
+Class Name
+Product Type
+Parent ID
+Is Multibox
+Item Volume
+Box Length
+Box Height
+Box Width
+Box Weight
+Product Status
+Current Inventory JSON
+current_0,
+current_add_1_month,
+current_add_2_month,
+current_add_3_month,
+current_add_4_month,
+current_add_5_month,
+current_add_6_month,
+current_add_7_month,
+current_add_8_month
+Additional Product Details (Nested)
+Bestchoice(BCP) Product Title 
+Bestchoice Overall Rating
+Bestchoice Total Ratings
+Bestchoice Rating Percentages
+Bestchoice Price
+Bestchoice Details: [List of features and descriptions]
+Bestchoice Best Seller Rank: [List of ranks in various categories]
+Bestchoice Positive Reviews Summary
+Bestchoice Critical Reviews Summary
+competitor product_title
+competitor overall_rating
+competitor total_ratings
+competitor Rating Percentages
+competitor price
+competitor Details : [List of features and descriptions]
+competitor best_seller_rank: [List of ranks in various categories]
+competitor Positive Reviews Summary
+competitor Critical Reviews Summary
+'''
+product_json1=''' 
+
+SKU
+Product Name
+Bestchoice(BCP) Product Title 
+Bestchoice Positive Reviews Summary
+Bestchoice Critical Reviews Summary
+competitor Positive Reviews Summary
+competitor Critical Reviews Summary
+'''
+categories = '''
+- Positive and Critical Reviews
+- Product Details
+- Forecast calculation
+'''
+
+product_json2=''' 
+Our products
+- Product name
+- product_title
+- reviews positive
+- reviews negative
+Competitors products
+- competitors product name
+- reviews positive
+- reviews negative
+
+
+
+'''
+categories = '''
+- Positive and Critical Reviews
+- Product Details
+- Forecast calculation
+'''
